0c1f9904bbdfcd8ba8ac4cce58c816e5ec2b3d66
Adam Bross <sharakouze@hotmail.com>
2016-09-06 21:42:05 +0200

    apphost iis

---
+++ Tmpi.Pyrene/Tmpi.Pyrene.Services/AppHost.cs
@@ -0,0 +1,53 @@
+﻿using Funq;
+using ServiceStack;
+using ServiceStack.Api.Swagger;
+using ServiceStack.Data;
+using ServiceStack.OrmLite;
+using ServiceStack.Text;
+using System;
+using System.Configuration;
+using Tmpi.Pyrene.Services.ServiceInterface;
+using Tmpi.Pyrene.Services.ServiceModel.Types;
+
+namespace Tmpi.Pyrene.Services
+{
+    public class AppHost : AppHostBase
+    {
+        public AppHost()
+            : base("ConsoleApplication1", typeof(GenCompteurService).Assembly) { }
+
+        public override void Configure(Container container)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["PyreneModel"].ConnectionString;
+            container.Register<IDbConnectionFactory>(c =>
+                        new OrmLiteConnectionFactory(connectionString, SqlServerDialect.Provider));
+
+            bool pluginSwagger = false;
+            bool.TryParse(ConfigurationManager.AppSettings["PluginSwagger"], out pluginSwagger);
+            if (pluginSwagger)
+            {
+                Plugins.Add(new SwaggerFeature());
+            }
+
+            OrmLiteConfig.InsertFilter = (dbCmd, row) =>
+            {
+                var auditRow = row as IAuditable;
+                if (auditRow != null)
+                {
+                    auditRow.DatCreation = auditRow.DatModif = DateTime.UtcNow;
+                }
+            };
+
+            OrmLiteConfig.UpdateFilter = (dbCmd, row) =>
+            {
+                var auditRow = row as IAuditable;
+                if (auditRow != null)
+                {
+                    auditRow.DatModif = DateTime.UtcNow;
+                }
+            };
+
+            JsConfig.DateHandler = ServiceStack.Text.DateHandler.ISO8601;
+        }
+    }
+}
--- Tmpi.Pyrene/Tmpi.Pyrene.Services/Properties/AssemblyInfo.cs
+++ Tmpi.Pyrene/Tmpi.Pyrene.Services/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Les informations générales relatives à un assembly dépendent de 
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
-[assembly: AssemblyTitle("Tmpi.Pyrene.Services.ServiceInterface")]
+[assembly: AssemblyTitle("Tmpi.Pyrene.Services")]
 [assembly: AssemblyDescription("")]
 
 // Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
-[assembly: Guid("813f65d3-22f1-41db-84fc-7cfffbead768")]
+[assembly: Guid("5d4f7102-8811-420e-930c-98cc13182d29")]
 
 // Les informations de version pour un assembly se composent des quatre valeurs suivantes :
 //
@@ -18,8 +18,7 @@ using System.Runtime.InteropServices;
 //      Numéro de build
 //      Révision
 //
-// Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut 
+// Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de révision et de build par défaut 
 // en utilisant '*', comme indiqué ci-dessous :
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyFileVersion("1.0")]
