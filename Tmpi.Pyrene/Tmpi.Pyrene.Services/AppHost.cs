ef9c7d65dbe4fcbdbee05b2bc049eb56345ed8fe
Adam Bross <sharakouze@hotmail.com>
2016-09-14 23:09:00 +0200

    config apphost

--- Tmpi.Pyrene/Tmpi.Pyrene.Services/AppHost.cs
+++ Tmpi.Pyrene/Tmpi.Pyrene.Services/AppHost.cs
@@ -1,19 +1,12 @@
 ﻿using Funq;
 using ServiceStack;
-using ServiceStack.Api.Swagger;
-using ServiceStack.Data;
-using ServiceStack.OrmLite;
-using ServiceStack.Text;
-using System;
-using System.Configuration;
-using Tmpi.Pyrene.Infrastructure.Types;
 
 namespace Tmpi.Pyrene.Services
 {
     public class AppHost : AppHostBase
     {
         public AppHost()
-            : base("Tmpi.Pyrene.Services", Licenses.GetAssembliesWithServices())
+            : base(AppHostHelper.ServiceName, Licenses.GetAssembliesWithServices())
         {
         }
 
@@ -30,38 +23,13 @@ namespace Tmpi.Pyrene.Services
                 EnableFeatures = currentFeatures.Remove(Feature.Soap),
             });
 
-            bool pluginSwaggerEnabled = false;
-            bool.TryParse(ConfigurationManager.AppSettings["PluginSwaggerEnabled"], out pluginSwaggerEnabled);
-            if (pluginSwaggerEnabled)
-            {
-                Plugins.Add(new SwaggerFeature());
-            }
+            AppHostHelper.ConfigCsv();
 
-            string connectionString = ConfigurationManager.ConnectionStrings["PyreneModel"].ConnectionString;
-            container.Register<IDbConnectionFactory>(c =>
-                        new OrmLiteConnectionFactory(connectionString, SqlServerDialect.Provider));
+            AppHostHelper.ConfigJson();
 
+            AppHostHelper.ConfigDbConnection(container, "PyreneModel");
 
-
-            OrmLiteConfig.InsertFilter = (dbCmd, row) =>
-            {
-                var auditRow = row as IAuditable;
-                if (auditRow != null)
-                {
-                    auditRow.DatCreation = auditRow.DatModif = DateTime.UtcNow;
-                }
-            };
-
-            OrmLiteConfig.UpdateFilter = (dbCmd, row) =>
-            {
-                var auditRow = row as IAuditable;
-                if (auditRow != null)
-                {
-                    auditRow.DatModif = DateTime.UtcNow;
-                }
-            };
-
-            JsConfig.DateHandler = ServiceStack.Text.DateHandler.ISO8601;
+            AppHostHelper.ConfigPlugins(Plugins);
         }
     }
 }
---
+++ Tmpi.Pyrene/Tmpi.Pyrene.Services/AppHostHelper.cs
@@ -0,0 +1,88 @@
+﻿using Funq;
+using ServiceStack;
+using ServiceStack.Api.Swagger;
+using ServiceStack.Data;
+using ServiceStack.OrmLite;
+using ServiceStack.Text;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using Tmpi.Pyrene.Infrastructure.Types;
+
+namespace Tmpi.Pyrene.Services
+{
+    public static class AppHostHelper
+    {
+        /// <summary>
+        /// Obtient le nom du service.
+        /// </summary>
+        public static string ServiceName
+        {
+            get
+            {
+                return "Tmpi.Pyrene.Services";
+            }
+        }
+
+        /// <summary>
+        /// Configure la sérialisation CSV.
+        /// </summary>
+        public static void ConfigCsv()
+        {
+            string itemDelimiterString = ConfigurationManager.AppSettings["CsvConfig.ItemDelimiterString"];
+            CsvConfig.ItemDelimiterString = itemDelimiterString ?? string.Empty;
+
+            string itemSeparatorString = ConfigurationManager.AppSettings["CsvConfig.ItemSeparatorString"];
+            if (!string.IsNullOrEmpty(itemSeparatorString))
+            {
+                CsvConfig.ItemSeperatorString = itemSeparatorString;
+            }
+        }
+
+        /// <summary>
+        /// Configure la sérialisation JSON.
+        /// </summary>
+        public static void ConfigJson()
+        {
+            JsConfig.DateHandler = DateHandler.ISO8601;
+        }
+
+        public static void ConfigDbConnection(Container container, string connectionStringName)
+        {
+            //SqlServerDialect.Provider.RegisterConverter<short>(new SqlServerSmallintConverter());
+
+            OrmLiteConfig.InsertFilter = (dbCmd, row) =>
+            {
+                var auditRow = row as IAuditable;
+                if (auditRow != null)
+                {
+                    auditRow.DatCreation = auditRow.DatModif = DateTime.UtcNow;
+                }
+            };
+
+            OrmLiteConfig.UpdateFilter = (dbCmd, row) =>
+            {
+                var auditRow = row as IAuditable;
+                if (auditRow != null)
+                {
+                    auditRow.DatModif = DateTime.UtcNow;
+                }
+            };
+
+            string connectionString = ConfigurationManager.ConnectionStrings[connectionStringName].ConnectionString;
+            container.Register<IDbConnectionFactory>(
+                c => new OrmLiteConnectionFactory(connectionString, SqlServerDialect.Provider)
+                );
+        }
+
+        public static void ConfigPlugins(List<IPlugin> plugins)
+        {
+            bool swaggerEnabled = false;
+            bool.TryParse(ConfigurationManager.AppSettings["Plugins.SwaggerEnabled"], out swaggerEnabled);
+            if (swaggerEnabled)
+            {
+                plugins.Add(new SwaggerFeature());
+            }
+        }
+    }
+}
