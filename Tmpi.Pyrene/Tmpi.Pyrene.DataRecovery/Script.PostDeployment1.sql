e6584ec005e75eace7a41c8c70e0aef7c421168c
Nicolas Boix <nicolas.boix@tmpi.fr>
2016-10-13 14:01:28 +0200

    re

--- Tmpi.Pyrene/Tmpi.Pyrene.DataRecovery/Script.PostDeployment1.sql
+++ Tmpi.Pyrene/Tmpi.Pyrene.DataRecovery/Script.PostDeployment1.sql
@@ -1,1 +1,18 @@
-﻿:r Script1.sql
+﻿SET XACT_ABORT ON;
+SET NOCOUNT ON;
+
+GO
+
+:setvar SourceSchemaName "[CG66_PYRENE_DLB].[SA_TMPI]"
+
+GO
+
+--
+-- FONCTIONS TEMP
+--
+
+:r TempFunctions.sql
+
+GO
+
+:r Script1.sql
