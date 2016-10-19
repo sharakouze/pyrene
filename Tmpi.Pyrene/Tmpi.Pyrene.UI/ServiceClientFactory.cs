26ecc07888820d9f5892b87c8576429a9b09cef9
Nicolas Boix <nicolas.boix@tmpi.fr>
2016-10-19 15:15:44 +0200

    appsettings

---
+++ Tmpi.Pyrene/Tmpi.Pyrene.UI/ServiceClientFactory.cs
@@ -0,0 +1,12 @@
+﻿using ServiceStack;
+
+namespace Tmpi.Pyrene.UI
+{
+    internal static class ServiceClientFactory
+    {
+        public static IServiceClient Create()
+        {
+            return new JsonHttpClient();
+        }
+    }
+}
