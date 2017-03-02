ce1339d98f1af3476a468bff8ad4a0a9f4925b9b
Adam Bross <sharakouze@hotmail.com>
2017-03-02 23:20:50 +0100

    t4

---
+++ Tmpi.Pyrene/Tmpi.Pyrene/src/service-client.js
@@ -0,0 +1,23 @@
+System.register(["servicestack-client"], function (exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var servicestack_client_1, ServiceClient;
+    return {
+        setters: [
+            function (servicestack_client_1_1) {
+                servicestack_client_1 = servicestack_client_1_1;
+            }
+        ],
+        execute: function () {
+            ServiceClient = (function () {
+                function ServiceClient(appConfig) {
+                    this.appConfig = appConfig;
+                    this.client = new servicestack_client_1.JsonServiceClient('this.appConfig.ApiEndpointUrl');
+                }
+                return ServiceClient;
+            }());
+            exports_1("ServiceClient", ServiceClient);
+        }
+    };
+});
+//# sourceMappingURL=service-client.js.map
